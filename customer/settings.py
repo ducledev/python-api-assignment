00d5f1e37a082c5dbb8046e318893fdb60212e03
Duc Le <admin@ducle.dev>
2020-01-21 18:04:42 +0700

    load settings from env

--- customer/settings.py
+++ customer/settings.py
@@ -1,12 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+load_dotenv()
+
 POSTGRESQL = {
     'engine': 'postgresql+psycopg2',
     'pool_size': 100,
     'debug': False,
-    'username': 'panpac',
-    'password': 'panpac',
-    'host': 'localhost',
+    'username': os.getenv('DB_USERNAME'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
     'port': 5432,
-    'db_name': 'customer',
+    'db_name': os.getenv('DB_NAME'),
 }
 
 SQLALCHEMY = {
@@ -15,7 +21,7 @@ SQLALCHEMY = {
 }
 
 JWT_CONFIG = {
-    'secret_key': 'eyJhbGciOiAiSFMyNTYiLCAidHlwIj',
-    'username': 'giga',
-    'password': 'giga'
+    'secret_key': os.getenv('JWT_SEC_KEY'),
+    'username': os.getenv('JWT_USERNAME'),
+    'password': os.getenv('JWT_PASSWORD')
 }
